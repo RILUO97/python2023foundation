4f1ec319a3e53a8c16946b610ada434915a88a38
python2023foundation <2363941562@qq.com>
2023-05-09 16:48:50 +0800

    变更

---
+++ main.py
@@ -0,0 +1,27 @@
+# # import ipaddress
+# #
+# # # 定义 IP 网络
+# # # net = ('192.168.10.0/24')
+# #
+# # # 遍历 IP 地址范围
+# # for ip in range(1,20):
+# #     print('2001:DB7:1::/64')
+#
+# domains = [
+#     "www.kaitao.cn",
+#     "world.taobao.com",
+#     "www.zhifuche.com",
+#     "ai.taobao.com",
+#     "www.taoqao.com",
+#     "www.tmall.com",
+#     "h5.m.taobao.com",
+#     "www.shwpbbs.com",
+#     "bangpai.taobao.com"
+# ]
+#
+# new_domains = []
+#
+# for domain in domains:
+#     new_domains.append(domain + "A")
+# for new_domains in new_domains:
+#     print(new_domains)
